62f8123da40c117c9223234c84427e0d93f7e150
Shwinayy <164042247+Shwinayy@users.noreply.github.com>
2026-02-19 20:36:18 -0500

    Update hw2.asm

--- hw2.asm
+++ hw2.asm
@@ -1,55 +1,55 @@
 .data
-array: .word 0,0,0,0,0,0,0,0,0,0
+ARRAY: .word 0,0,0,0,0,0,0,0,0,0
 
 .text
-.globl main
+.globl MAIN
 
-main:
+MAIN:
     li x8, 0          # initialize loop counter i = 0
-    la x9, array      # base address of array
+    la x9, ARRAY      # base address of array
 
-loop:
+LOOP:
     li x5, 10         # loop limit = 10
-    bge x8, x5, done  # stop when i reaches 10
+    bge x8, x5, DONE  # stop when i reaches 10
 
     mv x10, x8        # i into argument register
-    jal fibonacci     # call fibonacci(i)
+    jal FIBONACCI     # call fibonacci(i)
 
     slli x6, x8, 2    
     add  x7, x9, x6   # compute address of array[i]
     sw   x10, 0(x7)   # store fibonacci result 
 
     addi x8, x8, 1    # increment i
-    j loop            # repeat loop
+    j LOOP            # repeat loop
 
-done:
-    j done            # infinite loop to end program
+DONE:
+    j DONE            # infinite loop to end program
 
-fibonacci:
+FIBONACCI:
     addi x2, x2, -4   # make space on stack
     sw   x1, 0(x2)    # return address
 
     li x5, 1          # check base case valueis 1
-    ble x10, x5, base # if n <= 1 return n
+    ble x10, x5, BASE # if n <= 1 return n
 
     li x6, 0          # first number = 0
     li x7, 1          # second number = 1
     li x28, 2         # start counter at 2
 
-fib_loop:
-    bgt x28, x10, fib_end  # stop when counter > n
+F_LOOP:
+    bgt x28, x10, FIB_FINISH  # stop when counter > n
 
     add x29, x6, x7   # compute next fibonacci number
     mv  x6, x7        # shift second into first
     mv  x7, x29       # update second with new value
 
     addi x28, x28, 1  # increment loop counter
-    j fib_loop
+    j F_LOOP
 
-fib_end:
+FIB_FINISH:
     mv x10, x7        # move result into return register
 
-base:
+BASE:
     lw   x1, 0(x2)    # restore return address
     addi x2, x2, 4    # restore stack pointer
     ret               # return to caller
